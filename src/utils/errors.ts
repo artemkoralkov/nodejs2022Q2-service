8ea612acc1e056b7c0f23cd4e93b51cdf86e3a07
Koralkov Artem <artemkoralkov@gmail.com>
2022-07-14 21:49:54 +0300

    feat: implement errors messages file

---
+++ src/utils/errors.ts
@@ -0,0 +1,13 @@
+export const ERRORS = {
+  USER_NOT_FOUND: 'user not found',
+  WRONG_OLD_PASSWORD: 'wrong old password',
+  ARTIST_NOT_FOUND: 'artist not found',
+  TRACK_NOT_FOUND: 'track not found',
+  ALBUM_NOT_FOUND: 'album not found',
+  TRACK_DOESENT_EXIST: "track doesn't exist",
+  ALBUM_DOESENT_EXIST: "album doesn't exist",
+  ARTIST_DOESENT_EXIST: "artist doesn't exist",
+  TRACK_NOT_IN_FAVORITES: 'track not in favorites',
+  ARTIST_NOT_IN_FAVORITES: 'artist not in favorites',
+  ALBUM_NOT_IN_FAVORITES: 'album not in favorites',
+};
