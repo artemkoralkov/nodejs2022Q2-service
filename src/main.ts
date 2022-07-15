d80d5f35eacf5f10c5189f75e0ce24e75cf7a60b
Koralkov Artem <artemkoralkov@gmail.com>
2022-07-15 13:37:59 +0300

    feat: implement main file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join, dirname } from 'path';
+import { parse } from 'yaml';
+import { readFile } from 'fs/promises';
+import { SwaggerModule } from '@nestjs/swagger';
+import 'dotenv/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const rootDirname = dirname(__dirname);
+  const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
+  const document = parse(DOC_API);
+  SwaggerModule.setup('doc', app, document);
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
