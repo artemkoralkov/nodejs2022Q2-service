9fb3a3638ee9ceb0882282187852a80f9c81cf8d
Koralkov Artem <artemkoralkov@gmail.com>
2022-07-30 11:28:44 +0300

    feat: implement jwt strategy

---
+++ src/modules/auth/guards/jwt-auth.guard.ts
@@ -0,0 +1,17 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { IS_PUBLIC_KEY } from 'src/decorators/public';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  // canActivate(context: ExecutionContext) {
+  //   const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+  //     context.getHandler(),
+  //     context.getClass(),
+  //   ]);
+  //   if (isPublic) {
+  //     return true;
+  //   }
+  //   return super.canActivate(context);
+  // }
+}
