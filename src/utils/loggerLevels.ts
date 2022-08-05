aafa80f9fe2b340e7814e9783a8ef2da93e4021f
Koralkov Artem <artemkoralkov@gmail.com>
2022-08-05 20:16:17 +0300

    feat: add logger levels constant

---
+++ src/utils/loggerLevels.ts
@@ -0,0 +1,7 @@
+export const LOGGER_LEVELS = {
+  1: ['log'],
+  2: ['log', 'error'],
+  3: ['log', 'error', 'warn'],
+  4: ['log', 'error', 'warn', 'debug'],
+  5: ['log', 'error', 'warn', 'debug', 'verbose'],
+};
